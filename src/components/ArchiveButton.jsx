468366af5b90c7c104e70926077ecbdf255e0403
rivonnywulandari <rivonnywulandari19@gmail.com>
2024-12-18 10:53:26 +0700

    Updates

--- src/components/ArchiveButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
- 
-function ArchiveButton({ id, onArchive }) {
-  return <button className='note-item__archive' onClick={() => onArchive(id)}>Archive</button>
-  // return <button className='note-item__archive' onClick={() => onArchive(id)}><i class="fa-solid fa-folder-open"></i></button>
-}
- 
-export default ArchiveButton;
--- src/components/DeleteButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
- 
-function DeleteButton({ id, onDelete }) {
-  return <button className='note-item__delete' onClick={() => onDelete(id)}>Delete</button>
-  // return <button className='note-item__delete' onClick={() => onDelete(id)}><i class="fas fa-trash"></i></button>
-
-}
- 
-export default DeleteButton;
